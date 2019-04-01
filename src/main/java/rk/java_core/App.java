d40f7f8dd0262a125ebd75cc0c77bb209461f249
theranjitkumar <web.ranjitkumar@gmail.com>
2019-04-02 00:46:38 +0530

    jpa demo added

--- src/main/java/rk/java_core/App.java
+++ src/main/java/rk/java_core/App.java
@@ -1,16 +1,42 @@
 package rk.java_core;
 
+import java.util.List;
 import java.util.Properties;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( " JAVA CORE " );
-        
-        Properties p=new Properties();  
-        System.out.println( p.getProperty("user"));
-    
-    
-    }
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import rk.java_core.entity.Student;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println(" JAVA CORE ");
+
+		Properties p = new Properties();
+		System.out.println(p.getProperty("user"));
+
+//		Student Entity
+		Student student = new Student();
+		student.setSid(102);
+		student.setSname("ranjit");
+
+//        jpa demo
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPADemo");
+		EntityManager em = emf.createEntityManager();
+
+		// To save
+		em.getTransaction().begin();
+		em.persist(student);
+		em.getTransaction().commit();
+		System.out.println(student);
+
+		// To save end
+
+		// To get
+		Student s = em.find(Student.class, 111);
+		System.out.println(s);
+		// To get end
+
+	}
 }
--- src/main/java/rk/java_core/collection/ArrayListTest.java
+++ src/main/java/rk/java_core/collection/ArrayListTest.java
@@ -6,12 +6,17 @@ public class ArrayListTest {
 
 	public static void main(String[] args) {
 			
-			ArrayList<String> al = new ArrayList<String>();
+//			ArrayList<String> al = new ArrayList<String>();
+			
+			LinkedList<String> al = new LinkedList<String>();
+			
 			al.add("ranjit");
 			al.add("rajesh");
 			al.add("Umesh");
+			al.remove("rajesh");
 			al.add("Rakesh");
-			
+
+			al.add("Rakesh");
 			Collections.sort(al);
 			
 			for(String s: al){
